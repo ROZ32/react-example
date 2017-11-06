9e177362520b38f6a33db2deda41d6dd970a59ea
Oscar Sanchez <oscar.sanchez@laureate.net>
2017-11-05 22:44:48 -0600

    beggining of the react-router integration

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 // import {TodoList} from './components/todolist_demo/todolist';
 // import {CollapsibleContainer} from './components/collapsible_demo/collaps_container';
 // import {Fetch} from './components/fetch_demo/fetch';
 import {CurrenciesConverter} from './components/currencies_demo/currencies_converter';
-
 
 import './styles/app.scss';
 
